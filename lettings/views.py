6b537fbe517f83892af836145fc763a6f17006a3
Laurent Jouron <jouronlaurent@hotmail.com>
2024-01-30 18:25:27 +0100

    push for office

--- lettings/views.py
+++ lettings/views.py
@@ -3,6 +3,8 @@ import sentry_sdk
 from django.http import Http404
 from django.shortcuts import render
 from .models import Letting
+from django.template import loader
+from django.http import HttpResponse
 
 logger = logging.getLogger(__name__)
 
@@ -12,8 +14,10 @@ def index(request):
         lettings_list = Letting.objects.all()
     except Letting.DoesNotExist as e:
         raise Http404("There are not lettings") from e
+
+    template = loader.get_template("lettings/index.html")
     context = {"lettings_list": lettings_list}
-    return render(request, "lettings/index.html", context)
+    return HttpResponse(template.render(context, request))
 
 
 def letting(request, letting_id):
