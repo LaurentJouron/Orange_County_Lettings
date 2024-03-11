4cd699f69a45040c148c54df1d40522261488462
Laurent Jouron <l.jouron@epparfums.com>
2024-03-11 09:35:16 +0100

    push for home

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -137,7 +137,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, "static")
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+}
 
 STORAGES = {
     "staticfiles": {
